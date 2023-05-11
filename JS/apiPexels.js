c0f7116abee64fe5e02d6b542aa673ec4a2f3a26
MatthieuMarchand <matthieu.marchand@mmibordeaux.com>
2023-05-11 12:58:57 +0200

    controle images

--- JS/apiPexels.js
+++ JS/apiPexels.js
@@ -1,5 +1,7 @@
 const apiKey = 'MXffOa6Yi5Ixi2IVwvGmkY2oUEsQ9MNh9PjFVLZ1zSUzjWtXCf6ytZy5';
 
+let usedImages = [];
+
 function getRandomImage() {
     return fetch(`https://api.pexels.com/v1/curated?per_page=1&page=${Math.floor(Math.random() * 100) + 1}`, {
         headers: {
@@ -8,9 +10,15 @@ function getRandomImage() {
     })
         .then(response => response.json())
         .then(data => {
-            return data.photos[0].src.large;
-    })
+            const newImage = data.photos[0].src.large;
+            if (usedImages.includes(newImage)) {
+                return getRandomImage();
+            } else {
+                usedImages.push(newImage);
+                return newImage;
+            }
+        })
         .catch(error => {
             console.log('Une erreur s\'est produite:', error);
-    });
+        });
 }
--- JS/images.js
+++ JS/images.js
@@ -1,5 +1,3 @@
-
-
 let images = [];
 
 function loadRandomImages() {
