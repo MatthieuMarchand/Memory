f5f38b29f5ecfc23c6cd21cd73190db5b159f3af
loic leforestier <loic.leforestier@mmibordeaux.com>
2023-05-13 18:06:48 +0200

    affichage du score sur l'écran d'accueil

--- JS/menu.js
+++ JS/menu.js
@@ -3,6 +3,13 @@ let completedLevels = localStorage.length - 1;
 
 for (let i = 1; i <= completedLevels; i++) {
   let bouton = document.getElementById(`boutonNiveau${i}`);
+  let textScoreLevel = document.getElementById(`levelStat${i}`);
+  console.log(textScoreLevel);
+  let dataLevel = JSON.parse(localStorage.getItem(`level${i}Data`));
+  let levelTime = dataLevel.dataTime;
+  let levelClicks = dataLevel.dataCountClick;
+  console.log(levelClicks);
+  textScoreLevel.textContent = `Time : ${levelTime} Number of clicks : ${levelClicks}`;
   bouton.style.pointerEvents = "all";
   bouton.style.opacity = "100%"
 }
