baea2b319f702a53b5c749c578ca4339030a70aa
loic leforestier <loic.leforestier@mmibordeaux.com>
2023-05-13 18:12:04 +0200

    fixing null reference bug on menu

--- JS/menu.js
+++ JS/menu.js
@@ -2,14 +2,20 @@
 let completedLevels = localStorage.length - 1;
 
 for (let i = 1; i <= completedLevels; i++) {
+  let levelPlayed = true;
   let bouton = document.getElementById(`boutonNiveau${i}`);
   let textScoreLevel = document.getElementById(`levelStat${i}`);
   console.log(textScoreLevel);
   let dataLevel = JSON.parse(localStorage.getItem(`level${i}Data`));
   let levelTime = dataLevel.dataTime;
   let levelClicks = dataLevel.dataCountClick;
-  console.log(levelClicks);
-  textScoreLevel.textContent = `Time : ${levelTime} Number of clicks : ${levelClicks}`;
+  if (levelTime || levelClicks != null) {
+    textScoreLevel.textContent = `Time : ${levelTime} Number of clicks : ${levelClicks}`;
+  }
+  else {
+    textScoreLevel.innerHTML = "Jouez pour établir un score !";
+  }
+
   bouton.style.pointerEvents = "all";
   bouton.style.opacity = "100%"
 }
