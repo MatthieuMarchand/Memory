602d8fb3b74dcbf5acb36fab8791b06e583d3b71
MatthieuMarchand <matthieu.marchand@mmibordeaux.com>
2023-05-21 13:37:59 +0200

    update

--- JS/loader.js
+++ JS/loader.js
@@ -1,11 +1,11 @@
 const loader = document.getElementById('loader');
 
-// window.addEventListener('load', () => {
-//     setTimeout(function() {
-//         loader.classList.add('fonduOut');
+window.addEventListener('load', () => {
+    setTimeout(function() {
+        loader.classList.add('fonduOut');
 
-//         setTimeout(function() {
-//             loader.style.display = "none";
-//         }, 600);
-//     }, 2200);
-// })
+        setTimeout(function() {
+            loader.style.display = "none";
+        }, 600);
+    }, 2200);
+})
